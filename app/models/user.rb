fdfa645e862af66cbd4b4eb33f4761a0e937dabd
Daniel Helmick <dhelmick103@gmail.com>
2016-01-31 14:46:00 -0500

    Checkpoint 36 assignment validations complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # #2
   before_save { self.email = email.downcase }
+  before_save :format_name
 
  # #3
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,12 +12,23 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
  # #6
-   validates :email,
+  validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 100 },
              format: { with: EMAIL_REGEX }
 
  # #7
-   has_secure_password
- end
+  has_secure_password
+
+  def format_name
+    if name
+      name_array = []
+      name.split.each do |name_part|
+        name_array << name_part.capitalize
+      end
+
+      self.name = name_array.join(" ")
+    end
+  end
+end
