bc10e4f59103e0f0cf079e371d061819aabb4e31
Daniel Helmick <dhelmick103@gmail.com>
2016-02-09 18:41:27 -0500

    Checkpoint 41 comments complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  resources :posts, only: [] do
+
+    resources :comments, only: [:create, :destroy]
+
+  end
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
